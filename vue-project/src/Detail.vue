efe9beeb5427411f0867783fab959d2b46230589
Tvqk98 <itquocky98@gmail.com>
2023-03-13 10:36:01 +0700

    first commit

---
+++ vue-project/src/Detail.vue
@@ -0,0 +1,37 @@
+<template>
+    <Page>
+        <ActionBar flat="true">
+          <StackLayout orientation="horizontal">
+            <Image src="res://icon" width="40" height="40" verticalAlignment="center" @tap="$navigateBack(App)"/>
+            <Label text="Login" fontSize="24" verticalAlignment="center" />
+          </StackLayout>
+        </ActionBar>
+      <Label text="hello" />
+        
+    </Page>
+</template>
+
+<script >
+ import App from "./App";
+  export default {
+    data() {
+      return {   
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
